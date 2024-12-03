8530763c513fa7efb43ecd869d3d96f8a82c82f8
Shahira Lashin <lashinshahira@gmail.com>
2024-12-03 12:18:18 +0400

    Save local changes before merging

---
+++ lessons.js
@@ -0,0 +1,95 @@
+const courses = [
+    {
+        id: 1001,
+        subjectName: "Art",
+        location: "Abu Dhabi",
+        price: 150,
+        availableSpaces: 10,
+        rating: 4,
+        image: "images/art.jpg"
+    },
+    {
+        id: 1002,
+        subjectName: "Dance",
+        location: "Dubai",
+        price: 200,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/dance.jpg"
+    },
+    {
+        id: 1003,
+        subjectName: "Tennis",
+        location: "Dubai",
+        price: 250,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/tennis.jpg"
+    },
+    {
+        id: 1004,
+        subjectName: "Football",
+        location: "Sharjah",
+        price: 100,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/football.jpeg"
+    },
+    {
+        id: 1005,
+        subjectName: "Horse Riding",
+        location: "Ras Al Khaimah",
+        price: 300,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/horseriding.jpg"
+    },
+    {
+        id: 1006,
+        subjectName: "Sailing",
+        location: "Ras Al Khaimah",
+        price: 250,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/sailing.jpeg"
+    },
+    {
+        id: 1007,
+        subjectName: "Music",
+        location: "Ajman",
+        price: 225,
+        availableSpaces: 10,
+        rating: 4,
+        image: "images/piano.jpg"
+    },
+    {
+        id: 1008,
+        subjectName: "Baking",
+        location: "Sharjah",
+        price: 95,
+        availableSpaces: 10,
+        rating: 4,
+        image: "images/baking.jpg"
+    },
+    {
+        id: 1009,
+        subjectName: "Hiking",
+        location: "Fujairah",
+        price: 175,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/hiking.jpg"
+    },
+    {
+        id: 1010,
+        subjectName: "Snorkelling",
+        location: "Fujairah",
+        price: 250,
+        availableSpaces: 10,
+        rating: 5,
+        image: "images/snorkelling.jpg"
+    },
+];
+
+// Exporting courses for use in other files
+export { courses };
